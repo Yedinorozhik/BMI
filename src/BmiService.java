d6e438dd88b74cc306933c1f15b910b846ac76de
yedinorozhik <misterrpandaa@gmail.com>
2024-11-30 19:16:44 +0300

    Reformat

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(double height, double weigt) {
-    double bmi = weigt / (height * height);
-    return (int) bmi;
+        double bmi = weigt / (height * height);
+        return (int) bmi;
     }
 }
